86736e62e00f3a33b7c98a0e579f744c13617b66
duilio1337 <rlh531@gmail.com>
2013-09-19 10:09:36 -0700

    started paddle

--- Pong/src/pong/Paddle.java
+++ Pong/src/pong/Paddle.java
@@ -1,7 +1,14 @@
 package pong;
 
+import java.awt.Image;
 import jgame.GSprite;
+import jgame.ImageCache;
 
 public class Paddle extends jgame.GSprite {
 
+	public Paddle () {
+		
+		super(ImageCache.forClass(Pong.class).get("paddle.png"));
+		
+	}
 }
